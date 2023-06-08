d209e3893dcfd71257fb007bd5939627ea1aec29
juuwes <contatosilvajuliana@gmail.com>
2023-06-07 21:43:01 -0300

    Implementação do método sacar

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,3 +1,15 @@
 public class ContaCorrente {
 
+	public int numeroConta;
+	
+	public void sacar (double valor) {
+
+		if (valor <= saldoConta) {
+			saldoConta -= valor;
+
+		} else {
+		System.out.println("Saldo Insuficiente");
+
+		}
+	}
 }
